c15df58cad9d825b7cb52fff2ee063ca25c876d0
KampfTracer <Franciscogabrielaguilera@gmail.com>
2023-08-20 11:20:59 -0400

    Inicio de e-commerce Propiedades

---
+++ src/componentes/Navbar/index.js
@@ -0,0 +1,26 @@
+import './style.css'
+
+const Navbar = () => {
+    return (
+
+         <nav className="navContainer">
+             <ul>
+                <li>
+                    <a href="#"> Home</a>
+                 </li>
+                <li>
+                     <a href="#"> Propiedades de Lujos</a>
+                </li>
+                 <li>
+                    <a href="#"> Terrenos</a>
+                 </li>
+                <li>
+                    <a href="#"> Contacto</a>
+                </li>
+             </ul>
+        </nav>
+    )
+
+}
+
+export default Navbar
